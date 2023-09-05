b80926854db160f67ac7c7a3631d8f07e991d30f
RohitSawant0351 <rohits1547@gmail.com>
2023-09-05 13:33:58 +0530

    reverted deployed link

--- finurlapp/src/Redux/Func/Stashfin/Check_Eligibility.js
+++ finurlapp/src/Redux/Func/Stashfin/Check_Eligibility.js
@@ -4,7 +4,7 @@ export const handleStashfinEligibility = (formData) => async (dispatch) => {
   // dispatch({ type: "CHECK_ELIGIBILITY_FOR_STASHFIN_REQUEST" });
   try {
     return await axios
-      .post("http://localhost:4000/api/v1/stashfin/login-client", {
+      .post("https://api.finurl.in/api/v1/stashfin/login-client", {
         id: "20395df108eb4c7fb8d94b40f2fb6f8a",
         client_secret: "BD2y7zO9D9Bq",
       })
@@ -15,7 +15,7 @@ export const handleStashfinEligibility = (formData) => async (dispatch) => {
             payload: res.data.results,
           });
           return axios
-            .post("http://localhost:4000/api/v1/stashfin/dedupe", {
+            .post("https://api.finurl.in/api/v1/stashfin/dedupe", {
               email: formData.email,
               phone: formData.phone,
               token: res.data.results,
